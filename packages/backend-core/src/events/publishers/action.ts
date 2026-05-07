470042aa36bc3ecc162485eac41df7eadc966ff5
jvcalderon <jose@budibase.com>
2026-05-07 16:01:20 +0200

    Configure publishers and omit from Posthog

--- packages/backend-core/src/events/publishers/action.ts
+++ packages/backend-core/src/events/publishers/action.ts
@@ -2,8 +2,11 @@ import { publishEvent } from "../events"
 import {
   Event,
   ActionAutomationStepExecuted,
+  ActionAutomationStepFailed,
   ActionCrudExecuted,
+  ActionCrudFailed,
   ActionAiAgentExecuted,
+  ActionAiAgentFailed,
 } from "@budibase/types"
 
 async function automationStepExecuted(
@@ -13,11 +16,21 @@ async function automationStepExecuted(
   await publishEvent(Event.ACTION_AUTOMATION_STEP_EXECUTED, action, timestamp)
 }
 
+async function automationStepFailed(action: ActionAutomationStepFailed) {
+  await publishEvent(Event.ACTION_AUTOMATION_STEP_FAILED, action)
+}
+
 async function crudExecuted(
   action: ActionCrudExecuted,
   timestamp?: string | number
 ) {
   await publishEvent(Event.ACTION_CRUD_EXECUTED, action, timestamp)
+}
+
+function crudFailed(action: ActionCrudFailed) {
+  publishEvent(Event.ACTION_CRUD_FAILED, action).catch(err => {
+    console.error("crudFailed telemetry failed", { action, err })
+  })
 }
 
 function aiAgentExecuted(
@@ -29,8 +42,17 @@ function aiAgentExecuted(
   })
 }
 
+function aiAgentFailed(action: ActionAiAgentFailed) {
+  publishEvent(Event.ACTION_AI_AGENT_FAILED, action).catch(err => {
+    console.error("aiAgentFailed telemetry failed", { action, err })
+  })
+}
+
 export default {
   aiAgentExecuted,
+  aiAgentFailed,
   automationStepExecuted,
+  automationStepFailed,
   crudExecuted,
+  crudFailed,
 }
